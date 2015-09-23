b9b87f2441bf5024c91c5eafddbd977bfee61ffe
unknown <romekulum2308@gmail.com>
2015-09-23 16:32:52 +0300

    plot_kmf(), plot_bin_tree(): axis properties fixed

--- plotting.py
+++ plotting.py
@@ -15,29 +15,43 @@ def get_description(groups, node, root=dsc.root):
         if parent == root:
             s += unicode(get_edge_label(groups, parent, node))
         else:
-            s += unicode(''.join([get_edge_label(groups, parent, node), ' ; ']))
+            s += unicode(''.join([get_edge_label(groups, parent, node), ';']))
         node = parent
     return s
 
 
 def plot_kmf(group, desc):
     def get_pos_textbox(grp):
-        max_obs = max(grp.get_time_last_obs())
-        min_surv = min(grp.get_surv())
-        x = 0.7 * max_obs
-        y = 1 - 0.35 * (1 - min_surv)
-        return x, y
+        # max_obs = max(grp.get_time_last_obs())
+        # min_surv = min(grp.get_surv())
+        # x = 0.7 * max_obs
+        # y = 1 - 0.35 * (1 - min_surv)
+        # return x, y
+        return 0.01, 0.98  # relative coords
 
     plt.figure()
     ax = plt.subplot(111)
     group.kmf1.plot(ax=ax, ci_show=False)
     group.kmf2.plot(ax=ax, ci_show=False)
     ax.set_title(desc, bbox={'facecolor': 'white', 'pad': 5})
-    s = u'p-value: %f\npower:  %f' % (group.logrank.p_value, group.pwr)
+    ax.legend(bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=0.)
+    s = u'p-value: %f\npower:   %f' % (group.logrank.p_value, group.pwr)
     x_coord, y_coord = get_pos_textbox(group)
-    ylim_top, ylim_bot = 1, max(0, min(group.get_surv()) - 0.1)
-    ax.text(x_coord, y_coord, s, bbox={'facecolor': 'white', 'pad': 7})
+    max_timeline = max(group.get_time_last_obs())
+    xlim_right, xlim_left = 1.01 * max_timeline, 0
+    ylim_top, ylim_bot = 1 + 0.23, min(group.get_surv()) - 0.1
+    ax.text(x_coord, y_coord, s, bbox={'facecolor': 'white', 'pad': 5},
+            verticalalignment='top', horizontalalignment='left', transform=ax.transAxes)
+    # ax.annotate(s, xy=(x_coord, y_coord), xycoords='axes fraction',
+    #             xytext=(x_coord, y_coord), textcoords='axes fraction',
+    #             bbox={'facecolor': 'white', 'pad': 7})
+    ax.set_xlim(xlim_left, xlim_right)
     ax.set_ylim(ylim_bot, ylim_top)
+    ax.set_xlabel('Timeline (days)')
+    ax.set_ylabel('Survival')
+    plt.grid()
+    plt.show()
+    return ax
 
 
 def make_layout(groups, node_size, root=dsc.root):
@@ -84,3 +98,7 @@ def plot_bin_tree(groups, node_size, with_labels=False, node_shape='s'):
     edge_labels = make_edge_labels(groups)
     draw_networkx(groups, pos=pos, ax=ax, with_labels=with_labels, node_size=node_size, node_shape=node_shape)
     draw_networkx_edge_labels(groups, pos=pos, ax=ax, edge_labels=edge_labels)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    plt.show()
+    return ax
